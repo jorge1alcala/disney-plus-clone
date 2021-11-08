2d1a9a6a6c3c8a080e1cd4ba629c19ee0273a258
Jorge Alcala <jorge1alcala@msn.com>
2021-11-08 13:15:24 -0800

    getting new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import {Routes} from './react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Detail from './components/Detail';
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Home />
+
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
