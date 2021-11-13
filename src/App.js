46e0ef0ae57f5ae74f334e621f2008b15c31abc3
Jorge Alcala <58161878+jorge1alcala@users.noreply.github.com>
2021-11-13 13:16:47 -0800

    firebase v9 fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Detail from './components/Detail';
-import Login from './components/Login';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import { useEffect } from 'react';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Header />
-      <Routes>
-      <Route path='/detail' element={<Detail />} />
-      
-    
-      <Route path="/" element={ <Home />} />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
 
-      <Route path="/login" element={ <Login />} />
+          <Route path="/" element={<Home />} />
 
-   
-      
-      </Routes>
-    </Router>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,64 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import ImgSlader from './ImgSlader'
-import Movies from './Movies'
-import Viewers from './Viewers'
-import db from '../firebase'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import ImgSlader from "./ImgSlader";
+import Movies from "./Movies";
+import Viewers from "./Viewers";
+import { collection, getDocs } from "firebase/firestore"; 
+import db from "../firebase";
 
 function Home() {
+  // useEffect(() => {
+  //   db.collection("novies").onSnapshot((snapshot) => {
+  //     let tempMovies = snapshot.docs.map((doc) => {
+  //       console.log(doc.data());
+  //       return { id: doc.id, ...doc.data() };
+  //     });
+  //     console.log(tempMovies);
+  //   });
+  // });
+
+  // const asyncFunction = async () => {
+
+  //     const querySnapshot = await getDocs(collection(db, "movies"))
+  //       let tempMovies = querySnapshot.forEach((doc) => {
+  //         return { id: doc.id, ...doc.data()}
+  //       });
+  //  console.log(tempMovies);
+  //   }
 
   useEffect(() => {
-    db.collection("movies").onSnapshot((snapshot)=>{
-      let tempMovies = snapshot.docs.map((doc)=>{
-        console.log(doc.data());
-        return { id: doc.id, ...doc.data()}
-      })
-      console.log(tempMovies);
+
+    async function getMovies() {
+      const movieCol = collection(db, 'movies');
+      const moviesSnapshot = await getDocs(movieCol);
+      const tempMovies = moviesSnapshot.docs.map((doc) => {
+         return {id: doc.id, ...doc.data()};
     })
-  })
+    console.log(tempMovies);
+  };
+     getMovies();
+  }, [])
+   
   return (
     <Container>
       <ImgSlader />
       <Viewers />
       <Movies />
     </Container>
-  )
+  );
 }
 
-export default Home
+export default Home;
 
 const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
   overflow-x: hidden;
-  
+
   &:before {
-    background: url("/images/home-background.png") center center / cover 
-    no-repeat fixed;
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
     content: "";
     position: absolute;
     top: 0;
@@ -44,4 +67,4 @@ const Container = styled.main`
     bottom: 0;
     z-index: -1;
   }
-`
+`;
