415405f8013253c38bf9544f5e3c359ed6cadf34
Jorge Alcala <jorge1alcala@msn.com>
2022-11-19 11:39:45 -0800

    finched

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { selectMovies } from "../features/movies/movieSlice"
+import { selectMovies } from "../features/movies/movieSlice";
 import { useSelector } from "react-redux";
 
 function Movies() {
-
   const movies = useSelector(selectMovies);
   // console.log("this are movies", movies);
 
@@ -13,78 +12,28 @@ function Movies() {
     <Container>
       <h4>Recommended for You</h4>
       <Content>
-
-      { movies && 
+        {movies &&
           movies.map((movie) => (
             <Wrap key={movie.id}>
-            <Link to={`/detail/${movie.id}`}>
-              <img
-              src={movie.cardImg} alt={movie.title}/>
+              <Link to={`/detail/${movie.id}`}>
+                <img src={movie.cardImg} alt={movie.title} />
               </Link>
             </Wrap>
-          ))
-      }
-         
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
-        <Wrap>
-        <a href="/detail" >
-          <img
-            src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
-            alt="the simsoms"
-          />
-          </a>
-        </Wrap>
+          ))}
       </Content>
     </Container>
   );
 }
+
+/*
+<a href="/detail">
+<img
+  src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg"
+  alt="the simsoms"
+/>
+</a>
+</Wrap>
+*/
 
 export default Movies;
 
