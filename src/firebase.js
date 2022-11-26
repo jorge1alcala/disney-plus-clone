ea9aa511452d77071788ed72c8cf58d37269f0b7
Jorge Alcala <jorge1alcala@msn.com>
2022-11-26 12:30:54 -0800

    package json deleted

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore"
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvtSh7J09iUzNXT_uR0d_X1S-xXj6CqLQ",
@@ -12,15 +10,14 @@ const firebaseConfig = {
   storageBucket: "disneyplus-clone-714dc.appspot.com",
   messagingSenderId: "680409295786",
   appId: "1:680409295786:web:fcf3ab9e3f33680fef19d3",
-  measurementId: "G-ZW9NTTETV0"
+  measurementId: "G-ZW9NTTETV0",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
-const provider = new GoogleAuthProvider;
-const storage = getStorage(firebaseApp);
-
+// const provider = new GoogleAuthProvider;
+// const storage = getStorage(firebaseApp);
 
 export { auth };
 export default db;
